50b87fa784edbf1cf1e04053254e492b43fd3fc0
sky-monarch <cca06@mail.ru>
2025-11-07 18:53:26 +0300

    Добавленна физика падения и прыжок, а также бег персонажа

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,40 @@
 extends CharacterBody2D
 
-var speed = 400
+@export var speed: float = 400
+@export var gravity: float = 900
+@export var jump_force: float = 400
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-		var direction = Input.get_axis("left","right")
-		velocity.x = speed * direction
+	
+	# Обработка гравитации
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Горизонтальное движение
+	var direction = Input.get_axis("left", "right")
+	var isRun = Input.is_action_pressed("run")
+	velocity.x = direction * speed
+	if isRun:
+		velocity.x *= 2
+	
+
+	# Обработка прыжка
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+
+	# Выбор анимации
+	if not is_on_floor():
+		anim.play("Jump")
+	elif direction != 0:
 		anim.flip_h = direction < 0
-		anim.play("Walk")
+		if isRun:
+			anim.play("Run")
+		else:
+			anim.play("Walk")
 	else:
 		anim.play("Idle")
-		velocity.x = 0
+
 	move_and_slide()
+
 	
