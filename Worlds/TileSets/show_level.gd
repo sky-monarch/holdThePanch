1c982d4a2a641c42a41f0060f3a1472671417cd0
minay775 <dimaminaylenko06@gmail.com>
2025-11-28 20:30:23 +0300

    добавлена надпись с номером уровня, которая пропадает через 10 секунд

---
+++ Worlds/TileSets/show_level.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var level_label = $Label
+
+func _ready():
+	level_label.visible = true
+	
+	# Ждем 10 секунд
+	await get_tree().create_timer(10.0).timeout
+	
+	# Плавно исчезает за 1 секунду
+	var tween = create_tween()
+	tween.tween_property(level_label, "modulate:a", 0.0, 1.0)
+	await tween.finished
+	level_label.visible = false
