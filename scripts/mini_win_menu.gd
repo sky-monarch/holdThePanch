c0fa54e43a3df123a5c548c4499eb0b0ca580559
minay775 <dimaminaylenko06@gmail.com>
2025-11-29 21:47:39 +0300

    добавлены пока не функциональная сцена о разработчиках

--- scripts/mini_win_menu.gd
+++ scripts/mini_win_menu.gd
@@ -8,4 +8,4 @@ func _on_button_2_pressed():
 
 
 func _on_button_3_pressed() -> void:
-	get_tree(). change_scene_to_file("res://src/scenes_for_main_menu/main_menu.tscn")
+	get_tree(). change_scene_to_file("res://src/developers.tscn")
