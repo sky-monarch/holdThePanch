8074a623f18b5ef8949ecd6bdf83d279d2ed23c2
minay775 <dimaminaylenko06@gmail.com>
2025-11-29 21:31:54 +0300

    на меню победы плавно появляется текст

---
+++ src/rich_text_label.gd
@@ -0,0 +1,16 @@
+extends RichTextLabel
+@onready var text_label = $"."
+
+func _ready():
+	# Начальное состояние - невидим
+	text_label.visible = false
+	text_label.modulate.a = 0.0
+	
+	# Ждем 5 секунд
+	await get_tree().create_timer(1.0).timeout
+	
+	# Показываем и плавно появляемся
+	text_label.visible = true
+	
+	var tween = create_tween()
+	tween.tween_property(text_label, "modulate:a", 1.0, 1.0)
