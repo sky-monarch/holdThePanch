775f2d9066146f9cad01bfad4422e040ba700c0b
minay775 <dimaminaylenko06@gmail.com>
2025-11-29 22:29:16 +0300

    добавлена кнопка о разработчиках на главное меню

--- scripts/sripts_for_main_menu/main_menu.gd
+++ scripts/sripts_for_main_menu/main_menu.gd
@@ -23,3 +23,7 @@ func _on_exit_pressed() -> void:
 
 func _on_back_options_pressed() -> void:
 	_ready()
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/developers.tscn")
