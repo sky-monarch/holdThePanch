5489cbfc3ec650034bd163dfc677b6605100b85e
minay775 <dimaminaylenko06@gmail.com>
2025-11-29 21:28:00 +0300

    теперь кнопки на меню победы функциональные

---
+++ scripts/mini_win_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_button_2_pressed():
+	get_tree(). change_scene_to_file("res://src/scenes_for_main_menu/main_menu.tscn")
+	
+
+
+
+func _on_button_3_pressed() -> void:
+	get_tree(). change_scene_to_file("res://src/scenes_for_main_menu/main_menu.tscn")
