9d0b97fbbd5040c6f60f1e468c1bc64766c82c09
minay775 <dimaminaylenko06@gmail.com>
2025-12-06 21:19:58 +0300

    добавлен скрипт (manager_many) с логикой кнопок меню паузы для многопользовательской версии(остается соединить на карте, которая в данный момент в разработке)

---
+++ scripts/sripts_for_pause_menu/manager_many.gd
@@ -0,0 +1,29 @@
+extends Node
+@onready var pause_menu = $"../CanvasLayer/PauseMenu"
+var game_paused: bool = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		game_paused = !game_paused
+		
+	if game_paused == true:
+		get_tree().paused = true
+		pause_menu.show()
+	else:
+		get_tree().paused = false
+		pause_menu.hide()
+		
+
+
+func _on_button_pressed() -> void:
+	game_paused = !game_paused
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().paused = false
+	get_tree(). change_scene_to_file("res://src/scenes_for_main_menu/main_menu.tscn")
+
+# замена на сохранение игры.
+#func _on_button_2_pressed() -> void:
+#	get_tree().paused = false
+#	get_tree(). change_scene_to_file("res://Worlds/TileSets/world2.tscn")
