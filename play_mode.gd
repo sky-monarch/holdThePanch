5eadf8f8c43e7030ae2b7ff27e001c50dcaeb330
minay775 <dimaminaylenko06@gmail.com>
2025-12-06 19:59:39 +0300

    функционал кнопок добавлен

---
+++ play_mode.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Worlds/TileSets/world1.tscn")
+
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Worlds/TileSets/world1.tscn")
