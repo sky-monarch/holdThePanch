ec824c112ff436304fac85d92678ba9c516861e8
minay775 <dimaminaylenko06@gmail.com>
2025-12-06 20:29:46 +0300

    доработана меню выбора игрового режима(передано sky-monarch для добавления персонажей)

--- play_mode.gd
+++ play_mode.gd
@@ -7,3 +7,16 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Worlds/TileSets/world1.tscn")
+
+
+func _on_panel_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().change_scene_to_file("res://Worlds/TileSets/world1.tscn")
+			
+
+
+func _on_panel_2_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().change_scene_to_file("res://Worlds/TileSets/world1.tscn")
